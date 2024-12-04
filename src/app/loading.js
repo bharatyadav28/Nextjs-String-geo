c770ab20851896ac45d824e1c2c2121b4633b71d
Bharat Yadav <yadavbharat386@gmail.com>
2024-12-04 13:02:24 +0530

    Added loading on navigation

---
+++ src/app/loading.js
@@ -0,0 +1,8 @@
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return (
+    <div style={{ color: "white", height: "100vh", width: "100%" }}>
+      loading...
+    </div>
+  );
+}
