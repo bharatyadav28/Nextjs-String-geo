1b59dd19b00d7c3917a0f74ac3dd8e1e2d99bb14
Bharat Yadav <yadavbharat386@gmail.com>
2024-12-04 13:08:32 +0530

    Updated color for loader

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,7 +1,14 @@
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
   return (
-    <div style={{ color: "white", height: "100vh", width: "100%" }}>
+    <div
+      style={{
+        color: "black",
+        backgroundColor: "white",
+        height: "100vh",
+        width: "100%",
+      }}
+    >
       loading...
     </div>
   );
